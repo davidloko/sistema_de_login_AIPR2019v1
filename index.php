824115635cf697a43b8d42ba26ff049c411f1220
David Hoffmann <48031125+davidloko@users.noreply.github.com>
2019-07-19 14:48:15 -0300

    closes #2
    Formulário de cadastro de novo usuário

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                 </div>
             </section>
 
-            <section class="row">
+            <section class="row mb-5">
                 <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
                     <h2 class="text-center mt-2"> Entrada no Sistema</h2>    
                     <form action="#" id="formLogin" class="p-2">
@@ -48,7 +48,7 @@
                         </div>
 
                         <div class="form-group">
-                            <input type="submit" value="::Entrar::" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn block">
+                            <input type="submit" value="::Entrar::" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
                         </div>
 
                         <div class="form-group">
@@ -60,6 +60,56 @@
                     </form>            
                 </div>
             </section>
+    
+            <!--  Formulário de Cadastro -->
+            <section class="row mb-5">
+                <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaCadastro">
+                    <h2 class="text-center">Cadastro de Usuário</h2>
+                    <form action="#" class="p-2" id="frmCadastro">
+                    
+                        <div class="form-group">
+                            <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome Completo" required>
+                        </div>
+                    
+                        <div class="form-group">
+                            <input type="text" name="nomeUsuário" id="nomeUsuário" class="form-control" placeholder="Nome de Usuário" required>
+                        </div>
+                    
+                        <div class="form-group">
+                            <input  type="email" name="emailUsuário" id="emailUsuário" class="form-control" placeholder="E-mail de Usuário" required>
+                        </div>
+                    
+                        <div class="form-group">
+                            <input type="password" name="senhaUsuário" id="senhaUsuário" class="form-control" placeholder="Digite sua senha" required>
+                        </div>
+                    
+                        <div class="form-group">
+                        <input type="password" name="senhaConfirma" id="senhaConfirma" class="form-control" placeholder="Confirme sua senha" required>
+                        </div>
+                    
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
+                                <label for="concordar" class="custom-control-label">
+                                    Eu concordo com os <a href="#">termos e condições.</a>
+                                </label>
+                            </div>
+                        </div>
+                    
+                        <div class="form-group">
+                            <input type="submit" value=":: Cadastrar ::" class="btn btn-primary btn-block" id="btnRegistrar">
+                        </div>
+                            
+                        <div class="form-group">
+                            <p class="text-center">Já cadastrado? <a href="#" id="btnJaCadastrado">Entrar aqui.</a></p>
+                        </div>
+                    </form>
+
+                </div>  
+
+            </section>
+
+
         </main>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
