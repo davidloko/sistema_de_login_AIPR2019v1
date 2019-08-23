55fc5f318548162d6d02ec16cf7b019bf072752e
David Hoffmann <david_hoffmann@estudante.sc.senai.br>
2019-08-23 15:08:56 -0300

    Conexão com o banco de dados

---
+++ configDB.php
@@ -0,0 +1,19 @@
+<?php
+// configDB.php
+
+// Dados para escolha do database (DB)
+$dbhost = "localhost";
+$dbuser = "root"; // Usuário Raíz 
+$dbpass = "";
+$dbname = "sistemaDeLogin";
+
+// Conexão com o banco de dados
+
+$conecta = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if($conecta->connect_error){
+    die("Não foi possível conectar ao Banco de dados: " 
+            . $conexão->connect_error);}
+else{
+    echo "<h1>Conectou no BD Manowwwwww!</h1>";
+};
+
