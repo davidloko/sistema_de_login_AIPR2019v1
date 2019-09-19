0e48c0ca20e448c7bdb320ed7d12c7080c27fbde
David Hoffmann <48031125+davidloko@users.noreply.github.com>
2019-09-19 15:04:39 -0300

    Geração de Token
    1º passo para criar uma nova senha

--- recebe.php
+++ recebe.php
@@ -10,8 +10,29 @@ function verificar_entrada ($entrada) {
     $saida = htmlspecialchars($saida);
     return $saida;
 }
-if(
-    isset($_POST['action']) &&
+
+if(isset($_POST['action']) &&
+    $_POST['action'] == 'senha'){
+        // Apenas para debug
+        // echo "<strong>Recuperação de senha</strong>";
+        $emailSenha = verificar_entrada($_POST['emailSenha']);
+        $sql = $conecta->prepare("SELECT idUsuario FROM usuario WHERE email = ?");
+        $sql->bind_param("s", $emailSenha);
+        $sql->execute();
+        $resultado = $sql->get_result();
+        if($resultado->num_rows > 0){
+            // Existe o usuário no BD
+            // echo '<p class="text-success">E-mail encontrado</p>';
+            $frase = "aFsOmfkdDfjoAfdSfkE";
+            $frase_secreta = str_shuffle($frase);
+            $token = substr($frase_secreta,0,10);
+            echo "<p>$token</p>";
+        }else{
+            echo '<p class="text-danger">E-mail não encontrado</p>';
+        }
+
+
+}else if(isset($_POST['action']) &&
     $_POST['action'] == 'login'){
     // Verificação e login de usuário
     $nomeUsuario = verificar_entrada($_POST['nomeUsuario']);
